ba8a1e7e530e074dbe03fc1fab0a2329cd985e85
muhammadrizoprogrammer <mirzaolimovmuhammadrizo8@gmail.com>
2024-01-31 22:19:18 +0500

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+let PhonNumber = document.getElementById("PhonNumber");
+let Message = document.getElementById("Message");
+let footer_btn = document.getElementById("footer_btn");
+
+function myFunction() {
+
+    if (PhonNumber.value == "") {
+        PhonNumber.style.border = "5px solid red"
+    }
+    if(PhonNumber.value !== ""){
+        PhonNumber.style.border = "none"
+    }if(Message.value==""){
+        Message.style.border = "5px solid red"
+    }if(Message.value!==""){
+        Message.style.border = "none"
+    }
+
+}
